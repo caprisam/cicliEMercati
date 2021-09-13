fda59be2cea914a27443f4b150b8358b2c2d6284
sam <samy.m2000@gmail.com>
2021-09-13 22:19:10 +0200

    handle_update() now handling images

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request
 TELEGRAM_ENDPOINT = 'https://api.telegram.org/bot1973577571:AAEiEnEws5ADclz9aB9wWprcT_vs3kmBKSM'
 TELEGRAM_TOKEN = '1973577571:AAEiEnEws5ADclz9aB9wWprcT_vs3kmBKSM'
 
-TELEGRAM_CHANNEL = '@Cicli_e_Mercati' #'https://t.me/Cicli_e_Mercati'
+TELEGRAM_CHANNEL = '@Cicli_e_Mercati'
 
 
 app = Flask(__name__)
@@ -15,19 +15,31 @@ app = Flask(__name__)
 @app.route('/update/1973577571:AAEiEnEws5ADclz9aB9wWprcT_vs3kmBKSM', methods=['POST'])
 def handle_update():
     update = request.json
-    if 'message' in update:
-        send_channel_message(update['message'])
+    if 'image' in update:
+        send_channel_message(update['title'], update['message'], update['image'])
+    elif 'title' in update and 'image' not in update:
+        send_channel_message(update['title'], update['message'], 'None')
     return '{"status": "ok"}'
 
 
-def send_channel_message(message: str):
-    send_message(TELEGRAM_CHANNEL, message)
+def send_channel_message(title: str, message: str, image: str):
+    text = '*' + title + '*' + '\n\n' + message
+    if image == 'None':
+        send_message(TELEGRAM_CHANNEL, text)
+    else:
+        send_photo_with_text(TELEGRAM_CHANNEL, image, text)
 
 
 def send_message(chat_id, text):
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
     payload = {'chat_id': chat_id, 'text': text, 'parse_mode': 'Markdown'}
     requests.post(TELEGRAM_ENDPOINT + '/sendMessage', headers=headers, json=payload)
+
+
+def send_photo_with_text(chat_id, photo_url, message):
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    payload = {'chat_id': chat_id, 'photo': photo_url, 'caption': message, 'parse_mode': 'Markdown'}
+    requests.post(TELEGRAM_ENDPOINT + '/sendPhoto', headers=headers, json=payload)
 
 
 if __name__ == '__main__':
